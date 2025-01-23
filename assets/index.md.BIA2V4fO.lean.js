6250fbc887d3bea39fb20c3443927947c7a99c0a
kristijanhusak <husakkristijan@gmail.com>
2025-01-23 20:58:22 +0000

    Deploying to gh-pages from @ nvim-orgmode/nvim-orgmode.github.io@3853dfa5e4bfaa408f50aba4f713ebbccee8d2af 🚀

--- assets/index.md.BIA2V4fO.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,c as i,ae as s,o as t}from"./chunks/framework.BzDBnRMZ.js";const g=JSON.parse('{"title":"Nvim Orgmode","description":"","frontmatter":{},"headers":[],"relativePath":"index.md","filePath":"index.md"}'),o={name:"index.md"};function n(l,e,r,h,d,p){return t(),i("div",null,e[0]||(e[0]=[s("",20)]))}const k=a(o,[["render",n]]);export{g as __pageData,k as default};
