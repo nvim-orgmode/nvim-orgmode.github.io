c0105eac73944989ee16fd7620958cf2e608acb3
Kristijan Husak <husakkristijan@gmail.com>
2025-01-23 21:40:08 +0100

    Prepare deploy gh action

--- scripts/generate_md.mjs
+++ scripts/generate_md.mjs
@@ -7,8 +7,9 @@ import { fileURLToPath } from 'node:url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const generateMd = async () => {
-  const files = fs.readdirSync(path.resolve(__dirname, '../docs'))
-  fs.mkdirSync(path.resolve(__dirname, '../docs_md'))
+  const files = await fs.promises.readdir(path.resolve(__dirname, '../docs'))
+  await fs.promises.rm(path.resolve(__dirname, '../docs_md'), { recursive: true, force: true })
+  fs.promises.mkdir(path.resolve(__dirname, '../docs_md'))
   for await (const file of files) {
     execSync(`pandoc --lua-filter=${__dirname}/pandoc_fix_links.lua -s ${__dirname}/../docs/${file} -o ${__dirname}/../docs_md/${path.parse(file).name}.md -t gfm`)
   }
